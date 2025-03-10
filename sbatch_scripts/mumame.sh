46961604faad3dd357f7a88da1ad01bced30663b
Felix Blomfelt <155644281+blomfelt@users.noreply.github.com>
2025-03-10 19:45:41 +0100

    Update to new locations and add test files

--- sbatch_scripts/mumame.sh
+++ sbatch_scripts/mumame.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # SBATCH -A C3SE2025-1-11 -p vera
-# SBATCH -n 5
-# SBATCH -t 1:00:00
+# SBATCH -n 10
+# SBATCH -t 10:00:00
 # SBATCH -J mumame 
 # SBATCH --mail-user=blfelix@student.chalmers.se
 # SBATCH --mail-type=ALL
 # Set the names for the error and output files
-# SBATCH --error=/cephyr/NOBACKUP/groups/jbp-mv-waters/felix/logs/mumame.%J.err
-# SBATCH --output=/cephyr/NOBACKUP/groups/jbp-mv-waters/felix/logs/mumame.%J.out
+# SBATCH --error=/cephyr/NOBACKUP/groups/jbp-mv-waters/felix/logs/test_mumame.%J.err
+# SBATCH --output=/cephyr/NOBACKUP/groups/jbp-mv-waters/felix/logs/test_mumame.%J.out
 
 # ##
 #
@@ -16,6 +16,11 @@
 # Author: Felix Blomfelt
 #
 # Description: 
+# Runs mumame on the *val_1.fq.gz files present in $INDIR, using the database
+# located in $DATADIR constructed by mumame_build. 
+# It will automatically also use the corresponding *val_2.fq.gz file in the 
+# same directory as the first one.
+# It will output its *mapping_results files in $OUTDIR. 
 #
 # Usage: 
 # sbatch mumame.sh
@@ -28,13 +33,13 @@
 INDIR=/cephyr/NOBACKUP/groups/jbp-mv-waters/felix/storeDir/TrimGalore/;
 
 # Database directory
-DATADIR=/cephyr/NOBACKUP/groups/jbp-mv-waters/felix/Mumame_test/;
+DATADIR=/cephyr/NOBACKUP/groups/jbp-mv-waters/felix/Mumame_database/;
 
 # Output directory
-OUTDIR=/cephyr/NOBACKUP/groups/jbp-mv-waters/felix/Mumame_test/results;
+OUTDIR=/cephyr/NOBACKUP/groups/jbp-mv-waters/felix/Mumame_results;
 
 # Location of the container
-CONTAINER_LOC=/cephyr/NOBACKUP/groups/jbp-mv-waters/felix/Container/plastisphere_usearch64.sif;
+CONTAINER_LOC=/cephyr/NOBACKUP/groups/jbp-mv-waters/felix/Container/plastisphere.sif;
 
 # Temp files directory variable
 WORKING_TMP=$TMPDIR/MUMAME_TMP;
@@ -46,12 +51,13 @@ module purge
 mkdir $WORKING_TMP;
 cd $WORKING_TMP;
 # Copy relevant files to $TMPDIR
-cp $INDIR/SRR14061754_1_val_1.fq.gz $WORKING_TMP 
-#cp $INDIR/SRR3401475_1_val_1.fq.gz $WORKING_TMP 
 cp $DATADIR/mutation_database* $WORKING_TMP 
+# Total size approx 63 Gb, needs at least 5 cores for TMPDIR (845 Gb for 64 c)
+cp $INDIR/SRR1406*val_1.fq.gz $WORKING_TMP
+# DON'T FORGET TO CHANGE LOG NAME AND CORES ABOVE
 
 # Run mumame 
-apptainer exec $CONTAINER_LOC mumame -i SRR14061754_1_val_1.fq.gz -d "mutation_database_3.1.4" -o "mapping_results_3.1.4"
+apptainer exec $CONTAINER_LOC mumame -i *_1_val_1.fq.gz -d "mutation_database_3.1.4" -o "mapping_results_3.4"
 #apptainer exec $CONTAINER_LOC mumame -i "$INDIR/*val_1.fq.gz" -d mutation_database_3.4 -o "mapping_results"
 
 ### Copy all files back
