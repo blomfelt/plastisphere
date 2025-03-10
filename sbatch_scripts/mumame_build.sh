d04581fd323e84424c592b430eeed845d4ac9a13
Felix Blomfelt <155644281+blomfelt@users.noreply.github.com>
2025-03-10 19:44:34 +0100

    Update to new locations

--- sbatch_scripts/mumame_build.sh
+++ sbatch_scripts/mumame_build.sh
@@ -16,6 +16,10 @@
 # Author: Felix Blomfelt
 #
 # Description: 
+# Runs mumame_build on the card-database you define in $INDIR, and place the 
+# resulting mutation_database* files in $OUTDIR.
+# Here it will use protein_fasta_protein_variant_model.fasta and snps.txt
+# to create the database. 
 # 
 # Usage: 
 # sbatch mumame_build.sh
@@ -26,10 +30,10 @@
 ### Set parameters
 # Input directory
 #INDIR=/cephyr/NOBACKUP/groups/jbp-mv-waters/felix/card-data/;
-INDIR=/cephyr/NOBACKUP/groups/jbp-mv-waters/felix/card-data-3.1.4/;
+INDIR=/cephyr/NOBACKUP/groups/jbp-mv-waters/felix/card-data-3.4/;
 
 # Output directory
-OUTDIR=/cephyr/NOBACKUP/groups/jbp-mv-waters/felix/Mumame_test;
+OUTDIR=/cephyr/NOBACKUP/groups/jbp-mv-waters/felix/Mumame_database;
 
 # Location of the container
 CONTAINER_LOC=/cephyr/NOBACKUP/groups/jbp-mv-waters/felix/Container/plastisphere.sif;
@@ -48,7 +52,7 @@ cp $INDIR/protein_fasta_protein_variant_model.fasta $WORKING_TMP
 cp $INDIR/snps.txt $WORKING_TMP 
 
 # Run multiqc on all fastqc files in $INDIR
-apptainer exec $CONTAINER_LOC mumame_build -i protein_fasta_protein_variant_model.fasta -m snps.txt -d 16 -o "mutation_database_3.1.4"
+apptainer exec $CONTAINER_LOC mumame_build -i protein_fasta_protein_variant_model.fasta -m snps.txt -d 16 -o "mutation_database_3.4"
 
 ### Copy relevant files back
 cp -r $WORKING_TMP/mutation_database* $OUTDIR;
