8713f01de2b49a2a6c7d192ca4da642c1ed6f578
Felix Blomfelt <155644281+blomfelt@users.noreply.github.com>
2025-03-13 10:54:59 +0100

    Add jupyter run script

---
+++ sbatch_scripts/run_jupyter.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Time 
+TIME="01:00:00"			# Set the time you need to run Jupyter Lab
+
+ml purge
+
+# Container
+container=/cephyr/NOBACKUP/groups/jbp-mv-waters/felix/Container/plastisphere_jupyter.sif
+
+# You can launch jupyter notebook or lab: 
+srun -A "C3SE2025-1-11" -n 1 -t ${TIME} apptainer  exec  $container jupyter lab --ip=0.0.0.0 --port 8888
