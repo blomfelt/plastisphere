d0fb3e50f8c53c9503a3c2c60b7a24656d669c25
Felix Blomfelt <155644281+blomfelt@users.noreply.github.com>
2025-03-13 15:01:32 +0100

    Add better description

--- split_mumame.sh
+++ split_mumame.sh
@@ -10,6 +10,11 @@
 # beginning of the filenames which will be sent to mumame.sh.
 # This is done in order to be able to run several jobs in parallel, each unique
 # and not too large for the cores used. 
+# You may obtain a list of this form using:
+# ls *_1_val_1.fq.gz | sed -e "s/[0-9]\{2\}_1_val_1.fq.gz//" | uniq
+# And the size of each group may be found using
+# for file in $(ls *_1_val_1.fq.gz | sed -e "s/[0-9]\{2\}_1_val_1.fq.gz//" | \
+# uniq); do du -ch $file*gz | grep "total"; done
 #
 # Usage: 
 # split_mumame.sh
